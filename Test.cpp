969c64d87c6465c582e6d11f964a158c3ea069f4
Veil2311 <amuzak01@icloud.com>
2026-05-14 13:46:40 +0900

    nann

--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <string>
+
+class Greeter {
+public:
+	Greeter(std::string name, int id) : name_(std::move(name)), id_(id) {}
+
+	void Greet() const {
+		std::cout << "Hello, " << name_ << "!" << std::endl;
+	}
+	void GreetWithId() const {
+		std::cout << "Hello, " << name_ << " (ID: " << id_ << ")!" << std::endl;
+	}
+
+	int GetId() const {
+		return id_;
+	}
+
+    int id;
+
+
+private:
+	std::string name_;
+	int id_ = 0;
+};
 
 int main() {
-	std::cout << "Hello, C++!" << std::endl;
+	Greeter greeter("C++", 42);
+	greeter.Greet();
+	greeter.GreetWithId();
+	std::cout << "Hello, " << greeter.GetId() << "!" << std::endl;
+
+    std::cout << "Hello, " << greeter.id << "!" << std::endl;
+
 	return 0;
 }
